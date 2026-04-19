f00e9c58b0864878ceddcd348808a95af70e43a3
ulisestc <ulisestc27@gmail.com>
2026-04-19 01:02:54 -0600

    nose

--- Python/FastAPI/app.py
+++ Python/FastAPI/app.py
@@ -1,3 +1,29 @@
+import datetime
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Text
 
 app = FastAPI()
+
+posts = []
+#Post DTO
+class Post(BaseModel):
+    id: str
+    title: str
+    author: str
+    content: Text
+    created_at: datetime = datetime.now()
+    published_at: datetime
+    published : bool = False
+
+@app.get('/')
+def read_root():
+    return {"welcome": "Welcome to my rest API"}
+
+@app.get('/posts')
+def get_posts():
+    return posts
+
+@app.post('/posts')
+def save_post(post: Post):
+    print(post)
